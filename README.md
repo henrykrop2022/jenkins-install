67f7639f49ecd710697471261fe990c7ccac59f6
henrykrop2022 <henrykrop2022@gmail.com>
2023-01-18 14:42:45 -0600

    jenkins

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jenkins-install
+#---Jenkins installation on CentOS 7
+Jenkins is an open-source, Java-based automation server that offers an easy way to set up a continuous integration and continuous delivery (CI/CD) pipeline.
+Continuous integration (CI) is a DevOps practice in which team members regularly commit their code changes to the version control repository, after which automated builds and tests are run. 
+Continuous delivery (CD) is a series of practices where code changes are automatically built, tested and deployed to production
---
+++ installation.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+       #Author: --Henry
+      #Date: Jan 2023
+# ----Install Jenkins on centos 7 server----
+# 1.Step 1: Install Java & Enable the Jenkins repository
+sudo yum install java-11-openjdk-devel -y
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+# step 2. Install the latest stable version of Jenkins
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo systemctl enable jenkins
+# step 3. Adjust the firewall
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+sudo firewall-cmd --reload
+# step 4. utilities
+sudo yum install net-tools -y
+echo "ifconfig"
+
+#Setting up Jenkins in the browser
+   # 1. http://your_ip_or_domain:8080
+   # 2. sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+   # 3: Install suggested plugins
+#Create first admin user
+echo "installation complete"
